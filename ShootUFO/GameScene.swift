422a5207a2f0f722ebd1bc05b0e048bb82875174
Behlul Kusaslan <behlulkusaslan@gmail.com>
2017-12-27 13:50:27 +0300

    add alien

--- ShootUFO/GameScene.swift
+++ ShootUFO/GameScene.swift
@@ -15,19 +15,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var player: SKSpriteNode!
     var scoreLabel: SKLabelNode!
     
+    var possibleAliens = ["alien", "alien2", "alien3"]
     var score: Int = 0{
         didSet{
             scoreLabel.text = "Score: \(score)"
         }
     }
     
+    var gameTimer: Timer!
+    var WIDTH: CGFloat!
+    var HEIGHT: CGFloat!
+    
     override func didMove(to view: SKView) {
         
-        let WIDTH = self.frame.size.width
-        let HEIGHT = self.frame.size.height
+        WIDTH = self.frame.size.width
+        HEIGHT = self.frame.size.height
         
         starField = SKEmitterNode(fileNamed: "Starfield")
-        // TODO: find actual sizes for specific phone later
         starField.position = CGPoint(x: 0.0, y: HEIGHT)
         starField.advanceSimulationTime(10)
         starField.zPosition = -1
@@ -46,6 +50,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.fontSize = 42
         scoreLabel.fontColor = UIColor.white
         self.addChild(scoreLabel)
+        
+        gameTimer = Timer(timeInterval: 0.75, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
+    }
+    
+    @objc func addAlien() {
+        guard let shuffeldArray = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as? [String] else { return }
+        possibleAliens = shuffeldArray
+        
+        let alien = SKSpriteNode(imageNamed: possibleAliens[0])
+        let randomAlienPosition = GKRandomDistribution(lowestValue: Int(-WIDTH/2), highestValue: Int(WIDTH/2))
+        let positionX = CGFloat(randomAlienPosition.nextInt())
+        
+        alien.position = CGPoint(x: positionX, y: HEIGHT/2 - alien.size.height)
+        alien.physicsBody = SKPhysicsBody(rectangleOf: alien.size)
+        alien.physicsBody?.isDynamic = true
         
     }
     
